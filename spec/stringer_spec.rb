65a901cb795be9adab7fb0b4a73265fea7b504a6
rommelniebres <rommelniebresj@gmail.com>
2021-07-01 10:43:04 +0800

    first commit

---
+++ spec/stringer_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Stringer do
+  it "has a version number" do
+    expect(Stringer::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
